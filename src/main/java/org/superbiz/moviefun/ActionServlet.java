e8616c28cbdac176ddb91eb51a480391065ca9dd
Sathya <sathiasara@gmail.com>
2018-10-28 10:00:20 +0000

    Work in progress

--- src/main/java/org/superbiz/moviefun/ActionServlet.java
+++ src/main/java/org/superbiz/moviefun/ActionServlet.java
@@ -17,8 +17,10 @@
 package org.superbiz.moviefun;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
 
-import javax.ejb.EJB;
+
+//import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,14 +31,19 @@ import java.util.List;
 /**
  * @version $Revision$ $Date$
  */
+@Component
 public class ActionServlet extends HttpServlet {
 
     private static final long serialVersionUID = -5832176047021911038L;
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
+    //@EJB
     private MoviesBean moviesBean;
+
+    public ActionServlet(MoviesBean moviesBean){
+        this.moviesBean = moviesBean;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
